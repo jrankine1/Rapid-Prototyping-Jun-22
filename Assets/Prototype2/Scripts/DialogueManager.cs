9388a47be84168669a0604760705deaf79847ba6
jrankine1 <85931070+jrankine1@users.noreply.github.com>
2022-07-06 22:43:24 +1000

    Tree Start
    Added support to Unity for Ink file type, which is used to add dialogue to games, and set up the UI, currently having trouble with choices, Unity won't let them work for whatever reason

--- Assets/Prototype2/Scripts/DialogueManager.cs
+++ Assets/Prototype2/Scripts/DialogueManager.cs
@@ -1,15 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
 
 public class DialogueManager : MonoBehaviour
 {
-    public string[] sentences;
-    // Start is called before the first frame update
-    void Start()
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextAsset inkJson;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
+    private Story currentStory;
+
+    private bool dialogueIsPlaying;
+
+    public static DialogueManager instance;
+
+    private void Awake()
     {
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        EnterDialogueMode(inkJson);
+
+
         
     }
 
-    
+    public void EnterDialogueMode(TextAsset inkJson)
+    {
+        currentStory = new Story(inkJson.text);
+
+        ContinueStory();
+
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ContinueStory();
+        }
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            DisplayChoices();
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        //ti ensure the UI can support the number of choices coming in
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given then what the UI can support. Number of choices give " + currentChoices.Count);
+        }
+
+        int index = 0;
+        foreach (Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
 }
